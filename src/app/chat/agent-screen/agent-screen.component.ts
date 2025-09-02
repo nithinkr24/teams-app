1f575b941a2a4493a615f4ca4828d177d39b51e2
Nithin Chandran K R <nithin.chandran@expeed.com>
2025-09-02 12:59:23 +0530

    commit  getSalesRepInfo api

--- src/app/chat/agent-screen/agent-screen.component.ts
+++ src/app/chat/agent-screen/agent-screen.component.ts
@@ -129,7 +129,11 @@ export class AgentScreenComponent implements OnInit, OnDestroy {
       }
       
       console.log('Got Teams user info:', userInfo);
-
+      const getSalesRepInfo = await this.agentService.getSalesRepInfo(userInfo.objectId);
+      if (!getSalesRepInfo) {
+             this.errorMessage = 'Failed to get sales rep info';
+        return;
+      }
       const agentACSUser = await this.agentService.getAgentACSUser(userInfo.objectId);
       if (!agentACSUser) {
         this.errorMessage = 'Failed to link to ACS user';
